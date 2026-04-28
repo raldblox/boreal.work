6d063dd53e0ab5f009fc41bf974e59ae6ac026fa
raldblox <raldblox@outlook.com>
2026-04-29 07:37:59 +0800

    Hide public docs tabs from nav

--- next-app/components/home/public-site-nav-data.ts
+++ next-app/components/home/public-site-nav-data.ts
@@ -5,23 +5,7 @@ export type PublicSiteLink = {
 
 export const publicSiteLinks: PublicSiteLink[] = [
   {
-    href: "/about",
-    label: "About",
-  },
-  {
-    href: "/papers",
-    label: "Papers",
-  },
-  {
     href: "/roadmap",
     label: "Roadmap",
-  },
-  {
-    href: "/agents",
-    label: "Agent",
-  },
-  {
-    href: "/docs",
-    label: "Docs",
   },
 ]
